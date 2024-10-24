5896a05a95c9336c57e7faec164653ce98bc90a4
Unr1ll <x3narontyprojects@gmail.com>
2024-10-24 23:18:29 +0200

    Items list & prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,136 @@
 import {
-  Filters,
   Container,
+  Filters,
+  ProductsGroupList,
   Title,
   TopBar,
-  ProductCard,
 } from '@/components/shared';
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
-        <Title text="All pizzas" size="lg" className="font-extrabold" />
+         <Title text="All pizzas" size="lg" className="font-extrabold" /> 
       </Container>
-      <TopBar />
+       <TopBar /> 
 
       <Container className="pb-14 mt-10">
-        <div className="flex gap-[60px]">
-          {/*Filter*/}
+        <div className="flex gap-[80px]">
+          
           <div className="w-[250px]">
             <Filters />
           </div>
 
-          {/* Products list */}
+          
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductCard
-                id={0}
-                name="Cheeseburger-pizza"
-                price={0}
-                imageUrl="https://media.dodostatic.com/image/r:292x292/11EF16A483ADD64E91433787A0041619.avif"
+              <ProductsGroupList
+                title="Pizzas"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1}
+              />
+              <ProductsGroupList
+                title="Combos"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: 'Cheeseburger-pizza',
+                    imageUrl:
+                      'https://media.dodostatic.com/image/r:292x292/11EE87499F9A4542A46933A1B5FD8A0F.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={2}
               />
             </div>
           </div>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '@/store/category';
 import React from 'react';
 
 interface Props {
@@ -6,31 +9,33 @@ interface Props {
 }
 
 const cats = [
-  'Pizzas',
-  'Combos',
-  'Snacks',
-  'Cocktail',
-  'Coffee',
-  'Drinks',
-  'Deserts',
+  {id: 1, name: 'Pizzas',},
+  {id: 2, name: 'Combos',},
+  {id: 3, name: 'Snacks',},
+  {id: 4, name: 'Cocktail'},
+  {id: 5, name: 'Coffee',},
+  {id: 6, name: 'Drinks',},
+  {id: 7, name: 'Deserts'},
 ];
 const activeIndex = 0;
 
 export const Categories: React.FC<Props> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2x	l', className)}
     >
-      {cats.map((cat, index) => (
+      {cats.map(({name, id}, index) => (
     		<a className={cn(
             'flex items-center font-bold h-11 rounded-2xl px-5',
-            (activeIndex === index) &&
+            (categoryActiveId === id) &&
             'bg-white shadow-md shadow-gray-200 text-primary'
           )}
 					key={index}
+          href={`/#${name}`}
         >
 					<button>
-						{cat}
+						{name}
 					</button>
 				</a>
       ))}
